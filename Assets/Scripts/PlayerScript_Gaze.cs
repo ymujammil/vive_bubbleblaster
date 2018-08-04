2784a230c5a14032866f34383a2556bff8af0202
Mohamed Mujammil <mujammil@ualberta.ca>
2018-08-03 19:22:33 -0600

    Added PlayerControl with gaze

--- Assets/Scripts/PlayerScript_Gaze.cs
+++ Assets/Scripts/PlayerScript_Gaze.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class PlayerScript_Gaze : MonoBehaviour {
     Vector3 StandardViewPoint = new Vector3(0.5f, 0.5f, 10f);
@@ -8,6 +9,8 @@ public class PlayerScript_Gaze : MonoBehaviour {
     Camera SceneCamera;
     GameController controller;
     BubbleScript bubble;
+    VideoPlayer vplayer; 
+    
     //GameObject marker;
 
     private Vector2 GazeCenter;
@@ -17,6 +20,7 @@ public class PlayerScript_Gaze : MonoBehaviour {
     void Start () {
         SceneCamera = Camera.main;
         controller = GameObject.FindObjectOfType<Terrain>().GetComponent<GameController>();
+        vplayer = GameObject.FindGameObjectWithTag("Player").GetComponent<VideoPlayer>();
     }
     void OnEnable() {
         if (PupilTools.IsConnected)
@@ -39,17 +43,25 @@ public class PlayerScript_Gaze : MonoBehaviour {
             //Ray gaze3d = SceneCamera.ScreenPointToRay(marker.transform.position);
             //Debug.DrawRay(gaze3d.origin, gaze3d.direction, Color.green);
             RaycastHit hit;
-            if (Physics.Raycast(gazeray, out hit) && hit.collider.CompareTag("Bubble"))
+            if (Physics.Raycast(gazeray, out hit))
             {
-                GazeTime += Time.deltaTime;
-                if (GazeTime > 1.0f) {
-                    bubble = hit.collider.gameObject.GetComponent<BubbleScript>();
-                    bubble.SendMessage("Pop");
-                    GazeTime = 0.0f;
+                if (hit.collider.CompareTag("Bubble")) {
+                    GazeTime += Time.deltaTime;
+                    if (GazeTime > 1.0f)
+                    {
+                        bubble = hit.collider.gameObject.GetComponent<BubbleScript>();
+                        bubble.SendMessage("Pop");
+                        GazeTime = 0.0f;
+                    }
+                }
+
+                if (hit.collider.name == "Screen") {
+                    vplayer.Play();
                 }
             }
             else {
                 GazeTime = 0.0f;
+                vplayer.Pause();
             }
         }
 	}
